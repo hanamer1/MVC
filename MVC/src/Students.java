d25f5fd9a63f09dac5e4e05ce5d7ef0524294a65
Hana Amer <hanamer1@aucegypt.edu>
2022-12-26 10:36:52 +0900

    This is first commit in this project

---
+++ MVC/src/Students.java
@@ -0,0 +1,14 @@
+public class Students {
+
+    public static final Object[] TABLE_HEADER = {"Symbol", "Name"};
+
+    public static final Object[][] DATA = {
+            {"Math", "Amin"},
+            {"Computer Science", "Aya"},
+            {"Math", "Ali"},
+            {"Computer Science", "Hana"},
+            {"Computer Science", "Kenny"},
+            {"Math", "Mohamed"},
+            {"Computer Science", "Michael"},
+            {"Math", "Omar"}};
+}
